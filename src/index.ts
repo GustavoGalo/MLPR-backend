28d8e5eaf7f58de649c89aeaeec6608805dbb9d9
Gustavo Henrique Coimbra Galo <gg.gustavo.galo@gmail.com>
2021-11-11 23:19:51 -0300

    local database connection & use routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.URI || '', {});
+
 app.use(express.json());
+app.use(routes);
+
 app.listen(port, () => {
   console.log(`Server lintening in port ${port}`);
-})
+});
