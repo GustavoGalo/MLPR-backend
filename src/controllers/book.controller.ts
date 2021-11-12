4bf751b5d37d0d0e1e16eb7448d4bc1fb5526bb4
Gustavo Henrique Coimbra Galo <gg.gustavo.galo@gmail.com>
2021-11-11 23:18:31 -0300

    creatted book controller

---
+++ src/controllers/book.controller.ts
@@ -0,0 +1,39 @@
+import { BookModel, IBook } from '../models/book.model';
+
+class BookController {
+
+  private constructor () {}
+
+  private static _instance: BookController;
+
+  static get instance(): BookController {
+    if (!BookController._instance) {
+      BookController._instance = new BookController;
+    }
+    return this._instance;
+  }
+
+  public async ListAllBooks(): Promise<IBook[]> {
+    const books = await BookModel.find({});
+
+    return books;
+  }
+
+  public async ListBooksByTag(tag: string): Promise<IBook[]> {
+    const books = await BookModel.find({ tags: tag });
+
+    return books;
+  }
+
+  public async RegisterNewBook(book: IBook): Promise<IBook> {
+    const bookCreated = await BookModel.create(book);
+
+    return bookCreated;
+  }
+
+  public async DeleteBookById(id: string): Promise<void> {
+    await BookModel.findByIdAndDelete(id);
+  }
+}
+
+export default BookController;
