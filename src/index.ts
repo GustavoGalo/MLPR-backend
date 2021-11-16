d87c61c78bbc121c09fcdb40532c99f6c96ca193
Gustavo Henrique Coimbra Galo <gg.gustavo.galo@gmail.com>
2021-11-16 15:23:30 -0300

    swagger setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import swaggerUI from 'swagger-ui-express';
+
 import routes from './routes';
-import cors from 'cors';
+const SwaggerDocument = require('../swagger_output.json');
 
 dotenv.config();
 
@@ -14,6 +17,7 @@ mongoose.connect(process.env.URI || '', {});
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(SwaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server lintening in port ${port}`);
