c33c5887ff4a9bc43f1dba7b10ae5b36a31102e6
Gustavo Henrique Coimbra Galo <gg.gustavo.galo@gmail.com>
2021-11-16 13:48:05 -0300

    added cors in express & change default port to 5000

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.URI || '', {});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
